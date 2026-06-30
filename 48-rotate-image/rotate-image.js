5de6f1511db0590f9458063aedd22a2b6c01c23d
Rituraj Kumar Verma <riturajverma340@gmail.com>
2026-06-30 20:56:25 +0530

    Time: 0 ms (100.00%) | Memory: 54 MB (63.32%) - LeetSync

---
+++ 48-rotate-image/rotate-image.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var rotate = function(matrix) {
+     for(let i = 0; i < matrix.length; i++){
+        for(let j = i; j < matrix.length; j++){
+            let temp = matrix[i][j];
+            matrix[i][j] = matrix[j][i];
+            matrix[j][i] = temp;
+        }
+    }
+    for(let i = 0; i < matrix.length; i++){
+        let k = 0;
+        let j = matrix[i].length - 1;
+
+        while(k < j){
+            let temp = matrix[i][k];
+            matrix[i][k] = matrix[i][j];
+            matrix[i][j] = temp;
+
+            k++;
+            j--;
+        }
+    }
+};
