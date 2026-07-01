82abe0c4af9dd449ff48bb2d27d6e654b31cbe3c
Rituraj Kumar Verma <riturajverma340@gmail.com>
2026-07-01 19:36:26 +0530

    Time: 0 ms (100.00%) | Memory: 52.6 MB (95.35%) - LeetSync

---
+++ 74-search-a-2d-matrix/search-a-2d-matrix.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+var searchMatrix = function(matrix, target) {
+    for(i = 0; i < matrix.length; i++){
+        for(j = 0; j < matrix[i].length; j++){
+            if(matrix[i][j] === target){
+                return true
+            }
+        }
+       
+    }
+     return false
+};
