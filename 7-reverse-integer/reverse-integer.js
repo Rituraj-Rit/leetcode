b2d10f0f115e74e992123e1fc2acebd91bbe6d45
Rituraj Kumar Verma <riturajverma340@gmail.com>
2026-07-05 09:43:01 +0530

    Time: 49 ms (69.98%) | Memory: 55.4 MB (76.86%) - LeetSync

--- 7-reverse-integer/reverse-integer.js
+++ 7-reverse-integer/reverse-integer.js
@@ -2,20 +2,29 @@
  * @param {number} x
  * @return {number}
  */
-var reverse = function(x) {
-    let sign = x < 0 ? -1 : 1;
-    x = Math.abs(x);
+var reverse = function (x) {
+    let negative = false;
+
+    if (x < 0) {
+        negative = true;
+        x = -x;
+    }
 
     let rev = 0;
 
     while (x > 0) {
         let rem = x % 10;
-
-        if (rev > (2**31 - 1) / 10) return 0;
-
         rev = rev * 10 + rem;
         x = Math.floor(x / 10);
     }
 
-    return rev * sign;
+    if (negative) {
+        rev = -rev;
+    }
+    
+    if (rev < -(2 ** 31) || rev > 2 ** 31 - 1) {
+        return 0;
+    }
+
+    return rev;
 };
