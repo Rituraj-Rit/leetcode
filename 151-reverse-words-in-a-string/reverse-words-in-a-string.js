b5e6edd19a8362b52ee91af3e81aa2e2f4aadf61
Rituraj Kumar Verma <riturajverma340@gmail.com>
2026-07-09 18:54:42 +0530

    Time: 2 ms (58.71%) | Memory: 56.9 MB (29.91%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.js
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.js
@@ -6,6 +6,7 @@ var reverseWords = function (s) {
     let result = "";
     let i = s.length - 1;
 
+    // skip spaces
     while (i >= 0) {
         while (i >= 0 && s[i] === ' ') i--;
 
@@ -13,8 +14,11 @@ var reverseWords = function (s) {
 
         let j = i;
 
+        // find word
         while (j >= 0 && s[j] != ' ') j--;
 
+
+        // add word to result
         if (result.length === 0) {
             result += s.substring(j + 1, i + 1);
         } else {
