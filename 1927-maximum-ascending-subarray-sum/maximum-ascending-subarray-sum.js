c1a1afaa23e54f7a56bb41f404c99319b7da7c3c
Rituraj Kumar Verma <riturajverma340@gmail.com>
2026-07-15 20:49:08 +0530

    Time: 0 ms (100.00%) | Memory: 53.9 MB (35.48%) - LeetSync

---
+++ 1927-maximum-ascending-subarray-sum/maximum-ascending-subarray-sum.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxAscendingSum = function (nums) {
+    let maxSum = nums[0];
+    let currentSum = nums[0];
+
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] > nums[i - 1]) {
+            currentSum = currentSum + nums[i];
+        }else {
+            currentSum = nums[i]
+        }
+
+        maxSum = Math.max(maxSum, currentSum);
+    }
+    return maxSum;
+};
