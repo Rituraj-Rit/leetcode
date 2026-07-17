2a74d5959839de549ad802af4e820b18190370f2
Rituraj Kumar Verma <riturajverma340@gmail.com>
2026-07-17 20:10:35 +0530

    Time: 0 ms (100.00%) | Memory: 65.2 MB (7.59%) - LeetSync

---
+++ 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} arr
+ * @return {number[]}
+ */
+var replaceElements = function (arr) {
+    let prev = -1;
+    for (let i = arr.length - 1; i >= 0; i--) {
+        let cur = arr[i];
+        arr[i] = prev;
+        prev = Math.max(prev, cur);
+    }
+    return arr;
+};
