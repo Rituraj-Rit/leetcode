b5f7b96936878dd53afa2955066a95344d087171
Rituraj Kumar Verma <riturajverma340@gmail.com>
2026-07-19 13:34:56 +0530

    Time: 24 ms (33.14%) | Memory: 95.6 MB (83.24%) - LeetSync

---
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var rearrangeArray = function(nums) {
+    let ans = [], pos = 0, neg = 1;
+
+    for(let i = 0; i < nums.length; i++){
+        if(nums[i] < 0){
+            ans[neg] = nums[i];
+            neg += 2;
+        }else {
+            ans[pos] = nums[i];
+            pos += 2;
+        }
+    }
+    return ans;
+};
