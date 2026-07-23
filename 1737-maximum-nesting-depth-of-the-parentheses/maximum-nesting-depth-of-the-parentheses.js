93dbb5a004269e446faaf00591426c5777bf922b
Rituraj Kumar Verma <riturajverma340@gmail.com>
2026-07-23 13:09:25 +0530

    Time: 0 ms (100.00%) | Memory: 53.2 MB (88.57%) - LeetSync

---
+++ 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var maxDepth = function(s) {
+    let count = 0, maxCount = 0;
+    for(let i = 0; i <= s.length; i++){
+        if(s[i] === "(") count++;
+        if(s[i] === ")") count--;
+        maxCount = Math.max(maxCount, count);
+    }
+    return maxCount;
+};
