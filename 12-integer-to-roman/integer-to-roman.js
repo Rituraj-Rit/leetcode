f5d87873d952eb0daa9750dc27d60ee9ce56c347
Rituraj Kumar Verma <riturajverma340@gmail.com>
2026-07-31 11:41:56 +0530

    Time: 7 ms (40.69%) | Memory: 62.3 MB (39.07%) - LeetSync

---
+++ 12-integer-to-roman/integer-to-roman.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} num
+ * @return {string}
+ */
+var intToRoman = function(num) {
+    let symbols = [
+        [1000, 'M'],[900, 'CM'],[500, 'D'],[400, 'CD'], [100, 'C'], [90, 'XC'], [50, 'L'], [40, 'XL'], [10, 'X'], [9, 'IX'], [5, 'V'], [4, 'IV'], [1, 'I']
+    ];
+
+    let res = "";
+    for(let i = 0; i < symbols.length; i++){
+        let times = Math.floor(num / symbols[i][0]);
+        res = res + symbols[i][1].repeat(times);
+        num = num % symbols[i][0];
+    }
+    return res;
+};
