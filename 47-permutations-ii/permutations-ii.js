7faaf9646f63fccde48ed71d95bb0dacec628eb6
Rituraj Kumar Verma <riturajverma340@gmail.com>
2026-08-03 12:59:54 +0530

    Time: 2 ms (87.96%) | Memory: 59.5 MB (37.56%) - LeetSync

--- 47-permutations-ii/permutations-ii.js
+++ 47-permutations-ii/permutations-ii.js
@@ -9,37 +9,28 @@ let swapIndex = (arr, ind1, ind2) => {
     arr[ind2] = temp;
 };
 
-var permuteUnique = function (nums) {
-
-    let ans = [];
-    let len = nums.length;
-
+var permuteUnique = function(nums) {
+    let ans = [], len = nums.length;
     nums.sort((a, b) => a - b);
 
-    function generate(index) {
-
-        if (index === len) {
+    const generatePermutations = (index) => {
+        if (len === index) {
             ans.push([...nums]);
             return;
         }
 
-        let used = new Set();
-
+        let used = new Set();  
         for (let i = index; i < len; i++) {
-
             if (used.has(nums[i])) continue;
-
             used.add(nums[i]);
 
             swapIndex(nums, i, index);
-
-            generate(index + 1);
-
+            generatePermutations(index + 1);
             swapIndex(nums, i, index);
         }
-    }
+    };
 
-    generate(0);
+    generatePermutations(0);
 
     return ans;
 };
