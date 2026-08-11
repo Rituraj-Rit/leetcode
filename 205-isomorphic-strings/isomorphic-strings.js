b2d11bc0725c6f1ab3dc9690d45243710646ac86
Rituraj Kumar Verma <riturajverma340@gmail.com>
2026-08-11 15:13:35 +0530

    Time: 6 ms (69.99%) | Memory: 54.6 MB (71.75%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.js
+++ 205-isomorphic-strings/isomorphic-strings.js
@@ -3,18 +3,18 @@
  * @param {string} t
  * @return {boolean}
  */
-var isIsomorphic = function(s, t) {
-    if(s.length != t.length) return false;
+var isIsomorphic = function (s, t) {
+    if (s.length != t.length) return false;
 
     let map = new Map();
 
-    for(let i = 0; i < s.length; i++){
-        if(map.has(s[i])){
+    for (let i = 0; i < s.length; i++) {
+        if (map.has(s[i])) {
             let stored = map.get(s[i]);
-            if(stored !== t[i]) return false;
-        }else{
+            if (stored !== t[i]) return false;
+        } else {
             let alreadyTaken = [...map.values()].includes(t[i]);
-            if(alreadyTaken) return false;
+            if (alreadyTaken) return false;
             map.set(s[i], t[i]);
         }
     }
