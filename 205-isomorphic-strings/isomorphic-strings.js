1df16d1bda55d4ee2ae886286c0be801a4361b61
Rituraj Kumar Verma <riturajverma340@gmail.com>
2026-08-11 15:13:25 +0530

    Time: 7 ms (51.75%) | Memory: 55.1 MB (54.30%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isIsomorphic = function(s, t) {
+    if(s.length != t.length) return false;
+
+    let map = new Map();
+
+    for(let i = 0; i < s.length; i++){
+        if(map.has(s[i])){
+            let stored = map.get(s[i]);
+            if(stored !== t[i]) return false;
+        }else{
+            let alreadyTaken = [...map.values()].includes(t[i]);
+            if(alreadyTaken) return false;
+            map.set(s[i], t[i]);
+        }
+    }
+    return true;
+};
