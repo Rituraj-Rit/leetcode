77449ede10f4089b4caa37b47e1d0964ebdfc451
Rituraj Kumar Verma <riturajverma340@gmail.com>
2026-08-12 15:10:25 +0530

    Time: 0 ms (100.00%) | Memory: 53.7 MB (62.41%) - LeetSync

--- 162-find-peak-element/find-peak-element.js
+++ 162-find-peak-element/find-peak-element.js
@@ -2,21 +2,21 @@
  * @param {number[]} nums
  * @return {number}
  */
-var findPeakElement = function(nums) {
+var findPeakElement = function (nums) {
     let len = nums.length;
-    if(len === 1 ) return 0;
+    if (len === 1) return 0;
 
-    if(nums[0] > nums[1]) return 0;
-    if(nums[len-1] > nums[len - 2]) return len - 1;
+    if (nums[0] > nums[1]) return 0;
+    if (nums[len - 1] > nums[len - 2]) return len - 1;
 
-    let low = 1, high = len-2;
-    while(low <= high){
+    let low = 1, high = len - 2;
+    while (low <= high) {
         let mid = Math.floor((low + high) / 2);
-        if(nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1]){
+        if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1]) {
             return mid;
-        }else if(nums[mid] < nums[mid - 1]){
+        } else if (nums[mid] < nums[mid - 1]) {
             high = mid - 1;
-        }else low = mid + 1;
+        } else low = mid + 1;
     }
 
     return -1;
