4d9f508842fea6edf690ca33f1ccc952a980e617
Rituraj Kumar Verma <riturajverma340@gmail.com>
2026-08-27 23:06:48 +0530

    Time: 1 ms (27.27%) | Memory: 55.7 MB (69.70%) - LeetSync

---
+++ 342-power-of-four/power-of-four.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfFour = function(n) {
+    if (n <= 0) return false;
+
+    // n should be a power of 2
+    if ((n & (n - 1)) !== 0) return false;
+
+    // For power of 4, the 1-bit must be at an even position
+    return (n & 0x55555555) !== 0;
+};
