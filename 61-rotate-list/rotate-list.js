af672214af517006891fc0272aa4420612fb3536
Rituraj Kumar Verma <riturajverma340@gmail.com>
2026-09-02 14:10:03 +0530

    Time: 1 ms (21.59%) | Memory: 56.7 MB (65.10%) - LeetSync

---
+++ 61-rotate-list/rotate-list.js
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} k
+ * @return {ListNode}
+ */
+var rotateRight = function(head, k) {
+    if (!head || !head.next || k === 0) {
+        return head;
+    }
+
+    let arr = [];
+
+    let current = head;
+
+    while (current) {
+        arr.push(current.val);
+        current = current.next;
+    }
+
+    // Extra rotations remove
+    k = k % arr.length;
+
+    // Right rotate
+    for (let i = 0; i < k; i++) {
+        let last = arr.pop();
+        arr.unshift(last);
+    }
+
+    let newHead = new ListNode(arr[0]);
+    current = newHead;
+
+    for (let i = 1; i < arr.length; i++) {
+        current.next = new ListNode(arr[i]);
+        current = current.next;
+    }
+
+    return newHead;
+};
