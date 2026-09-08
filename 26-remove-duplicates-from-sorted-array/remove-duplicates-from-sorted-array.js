849f805661a9bedceeac7eb34e09e2d1caa7ce45
Rituraj Kumar Verma <riturajverma340@gmail.com>
2026-09-08 21:59:01 +0530

    Time: 0 ms (100.00%) | Memory: 58.3 MB (38.35%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
@@ -2,13 +2,21 @@
  * @param {number[]} nums
  * @return {number}
  */
-var removeDuplicates = function(nums) {
-    let unique = 0;
-    for(let i = 0; i < nums.length; i++){
-        if(nums[i] != nums[unique]){
-            unique++;
-            nums[unique] = nums[i];
+var removeDuplicates = function (nums) {
+    let j = 0;
+    let i = 1;
+    let rm = 1;
+    let n = nums.length;
+
+    while (i < n) {
+        if (nums[i] === nums[i - 1]) {
+            i++;
+            continue;
         }
+        nums[rm] = nums[i];
+        rm++;
+        i++;
     }
-    return unique + 1
+
+    return rm;
 };
