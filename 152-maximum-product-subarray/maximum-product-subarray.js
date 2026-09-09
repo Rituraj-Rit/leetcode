3309b934d82c34c640f4e5e61731183723b41c66
Rituraj Kumar Verma <riturajverma340@gmail.com>
2026-09-09 12:21:11 +0530

    Time: 6 ms (25.14%) | Memory: 57.9 MB (15.78%) - LeetSync

---
+++ 152-maximum-product-subarray/maximum-product-subarray.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxProduct = function(nums) {
+    let max = nums[0];
+    let min = nums[0];
+    let result = nums[0];
+
+    for (let i = 1; i < nums.length; i++) {
+        let curr = nums[i];
+
+        if (curr < 0) {
+            [max, min] = [min, max];
+        }
+
+        max = Math.max(curr, max * curr);
+        min = Math.min(curr, min * curr);
+
+        result = Math.max(result, max);
+    }
+
+    return result;
+};
