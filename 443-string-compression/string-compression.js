c6f8666b5d15f08f813db824254fcbb372729cfd
Rituraj Kumar Verma <riturajverma340@gmail.com>
2026-09-14 15:39:54 +0530

    Time: 1 ms (86.44%) | Memory: 58.2 MB (48.21%) - LeetSync

---
+++ 443-string-compression/string-compression.js
@@ -0,0 +1,32 @@
+/**
+ * @param {character[]} chars
+ * @return {number}
+ */
+var compress = function(chars) {
+    let write = 0;
+    let i = 0;
+
+    while (i < chars.length) {
+        let current = chars[i];
+        let count = 0;
+
+        while (i < chars.length && chars[i] === current) {
+            i++;
+            count++;
+        }
+
+        chars[write] = current;
+        write++;
+
+        if (count > 1) {
+            let str = count.toString();
+
+            for (let j = 0; j < str.length; j++) {
+                chars[write] = str[j];
+                write++;
+            }
+        }
+    }
+
+    return write;
+};
