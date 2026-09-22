8a3c8697eef7d87c57215c0ef379f04e6119836a
Rituraj Kumar Verma <riturajverma340@gmail.com>
2026-09-22 07:46:41 +0530

    Time: 1 ms (84.52%) | Memory: 56.5 MB (26.51%) - LeetSync

---
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersect = function(nums1, nums2) {
+    let map = new Map();
+    let ans = [];
+
+    for (let num of nums1) {
+        map.set(num, (map.get(num) || 0) + 1);
+    }
+
+    for (let num of nums2) {
+        if (map.get(num) > 0) {
+            ans.push(num);
+            map.set(num, map.get(num) - 1);
+        }
+    }
+
+    return ans;
+};
